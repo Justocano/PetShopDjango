e0835320a4b89d2d30d5f171a9ce387479474f17
Justo Cano <ju.cano@duouc.cl>
2023-06-27 22:23:58 -0400

    Fue acualizado

--- core/urls.py
+++ core/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 from .views import inicio,registro,bodega,boleta,carrito,ficha,ingresar,miscompras,misDatos,nosotros,_productos,ropa,usuarios,ventas,poblar,cambiar_estado_boleta
-from .views import eliminar_producto_en_bodega
+from .views import eliminar_producto_en_bodega, eliminar_producto_en_bodega,  eliminar_producto_en_carrito, agregar_producto_al_carrito
 
 urlpatterns = [
     path('', inicio, name="inicio"),
     path('registro',registro,name="registro"),
     path('bodega',bodega ,name="bodega"),
-    path('boleta', boleta,name="boleta"),
+    path('boleta/<nro_boleta>', boleta,name="boleta"),
     path('carrito', carrito,name="carrito"),
-    path('ficha',ficha ,name="ficha"),
+    path('ficha/<producto_id>',ficha ,name="ficha"),
     path('ingresar',ingresar ,name="ingresar"),
     path('miscompras',miscompras ,name="miscompras"),
     path('misDatos',misDatos ,name="misDatos"),
@@ -17,7 +17,9 @@ urlpatterns = [
     path('ropa',ropa ,name="ropa"),
     path('usuarios', usuarios,name="usuarios"),
     path('ventas',ventas ,name="ventas"),
-    path('cambiar_estado_boleta/<nro_boleta>/<estado>', cambiar_estado_boleta, name='cambiar_estado_boleta'),
     path('poblar',poblar,name="poblar"),
-    path('eliminar_producto_en_bodega/<bodega_id>', eliminar_producto_en_bodega, name='eliminar_producto_en_bodega')
+    path('cambiar_estado_boleta/<nro_boleta>/<estado>', cambiar_estado_boleta, name='cambiar_estado_boleta'),
+    path('eliminar_producto_en_bodega/<bodega_id>', eliminar_producto_en_bodega, name='eliminar_producto_en_bodega'),
+        path('eliminar_producto_en_carrito/<carrito_id>', eliminar_producto_en_carrito, name='eliminar_producto_en_carrito'),
+    path('agregar_producto_al_carrito/<producto_id>', agregar_producto_al_carrito, name='agregar_producto_al_carrito'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,12 +1,14 @@
 # Create your views here.
 from django.shortcuts import  render, redirect
 from datetime import date
+from django.contrib.auth.models import User
 from .poblar import poblar_bd
-from .models import Producto,Perfil,Bodega,DetalleBoleta, Boleta
+from .models import Producto,Perfil,Bodega,DetalleBoleta, Boleta,Carrito
 from .forms import ProductoForm, BodegaForm, RegistroClienteForm, IngresarForm
 from django.contrib import messages
 from django.contrib.auth import login, logout, authenticate
 from .tools import eliminar_registro, verificar_eliminar_registro
+from core.templatetags.custom_filters import formatear_dinero, formatear_numero
 # from .models import Mascota
 
 # Create your views here.
@@ -106,14 +108,107 @@ def eliminar_producto_en_bodega(request, bodega_id):
     return redirect(bodega)
 
 
-def boleta(request):
-    return render(request, "core/boleta.html")
+def boleta(request, nro_boleta):
+    boleta = Boleta.objects.get(nro_boleta=nro_boleta)
+    detalle_boleta = DetalleBoleta.objects.filter(boleta=boleta)
+    datos = { 
+        'boleta': boleta, 
+        'detalle_boleta': detalle_boleta 
+    }
+    return render(request, 'core/boleta.html', datos)
 
 def carrito(request):
     return render(request, "core/carrito.html")
 
-def ficha(request):
-    return render(request, "core/Ficha.html")
+def ficha(request,producto_id):
+    context = obtener_info_producto(producto_id)
+    return render(request, 'core/Ficha.html', context)
+
+
+def obtener_info_producto(producto_id):
+
+    producto = Producto.objects.get(id=producto_id)
+    stock = Bodega.objects.filter(producto_id=producto_id).exclude(detalleboleta__isnull=False).count()
+    
+    # Preparar texto para mostrar estado: en oferta, sin oferta y agotado
+    con_oferta = f'<span class="text-primary"> EN OFERTA {producto.descuento_oferta}% DE DESCUENTO </span>'
+    sin_oferta = '<span class="text-success"> DISPONIBLE EN BODEGA </span>'
+    agotado = '<span class="text-danger"> AGOTADO </span>'
+
+    if stock == 0:
+        estado = agotado
+    else:
+        estado = sin_oferta if producto.descuento_oferta == 0 else con_oferta
+
+    # Preparar texto para indicar cantidad de productos en stock
+    en_stock = f'En stock: {formatear_numero(stock)} {"unidad" if stock == 1 else "unidades"}'
+   
+    return {
+        'id': producto.id,
+        'nombre': producto.nombre,
+        'descripcion': producto.descripcion,
+        'imagen': producto.imagen,
+        'html_estado': estado,
+        'html_precio': obtener_html_precios_producto(producto),
+        'html_stock': en_stock,
+    }
+
+def eliminar_producto_en_carrito(request, carrito_id):
+    Carrito.objects.get(id=carrito_id).delete()
+    return redirect(carrito)
+
+
+
+def agregar_producto_al_carrito(request, producto_id):
+    perfil = request.user.perfil
+    producto = Producto.objects.get(id=producto_id)
+    precio_normal, precio_oferta, precio_subscr, hay_desc_oferta, hay_desc_subscr = calcular_precios_producto(producto)
+    precio = producto.precio
+    descuento_subscriptor = producto.descuento_subscriptor if perfil.subscrito else 0
+    descuento_total=producto.descuento_subscriptor + producto.descuento_oferta if perfil.subscrito else producto.descuento_oferta
+    precio_a_pagar = precio_subscr if perfil.subscrito else precio_oferta
+    descuentos = precio - precio_subscr if perfil.subscrito else precio - precio_oferta
+    Carrito.objects.create(
+        cliente=perfil,
+        producto=producto,
+        precio=precio,
+        descuento_subscriptor=descuento_subscriptor,
+        descuento_oferta=producto.descuento_oferta,
+        descuento_total=descuento_total,
+        descuentos=descuentos,
+        precio_a_pagar=precio_a_pagar
+    )
+    return redirect(ficha, producto_id)
+
+
+def calcular_precios_producto(producto):
+    precio_normal = producto.precio
+    precio_oferta = producto.precio * (100 - producto.descuento_oferta) / 100
+    precio_subscr = producto.precio * (100 - (producto.descuento_oferta + producto.descuento_subscriptor)) / 100
+    hay_desc_oferta = producto.descuento_oferta > 0
+    hay_desc_subscr = producto.descuento_subscriptor > 0
+    return precio_normal, precio_oferta, precio_subscr, hay_desc_oferta, hay_desc_subscr
+
+
+def obtener_html_precios_producto(producto):
+    
+    precio_normal, precio_oferta, precio_subscr, hay_desc_oferta, hay_desc_subscr = calcular_precios_producto(producto)
+    
+    normal = f'Normal: {formatear_dinero(precio_normal)}'
+    tachar = f'Normal: <span class="text-decoration-line-through"> {formatear_dinero(precio_normal)} </span>'
+    oferta = f'Oferta: <span class="text-success"> {formatear_dinero(precio_oferta)} </span>'
+    subscr = f'Subscrito: <span class="text-danger"> {formatear_dinero(precio_subscr)} </span>'
+
+    if hay_desc_oferta > 0:
+        texto_precio = f'{tachar}<br>{oferta}'
+    else:
+        texto_precio = normal
+
+    if hay_desc_subscr > 0:
+        texto_precio += f'<br>{subscr}'
+
+    return texto_precio
+    
 
 def ingresar(request):
 
@@ -136,7 +231,26 @@ def ingresar(request):
 
 
 def miscompras(request):
-    return render(request, "core/miscompras.html")
+    
+    usuario = User.objects.get(username='eolsen')
+    perfil = Perfil.objects.get(usuario=usuario)
+
+    boletas = Boleta.objects.filter(cliente=perfil)
+    historial =[]
+    for boleta in boletas:
+        boleta_historial = {
+            'nro_boleta': boleta.nro_boleta,
+            'fecha_venta': boleta.fecha_venta,
+            'fecha_despacho': boleta.fecha_despacho,
+            'fecha_entrega': boleta.fecha_entrega,
+            'total_a_pagar': boleta.total_a_pagar,
+            'estado': boleta.estado,
+        }
+        historial.append(boleta_historial)
+    return render(request, 'core/miscompras.html', { 
+        'historial': historial 
+    })
+
 
 
 def nosotros(request):
